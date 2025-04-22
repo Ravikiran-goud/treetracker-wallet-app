0886cb6a45a4f52e3032d08843de6b600801a6dc
Ravi Kiran Goud Jaggari <122633050+Ravikiran-goud@users.noreply.github.com>
2025-04-22 12:33:35 -0400

    chore: fixed and made final changes

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,32 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { Redirect, useRouter } from "expo-router";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-
-// export default function InitialRoute() {
-//   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(false);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const verifyAppLaunchStatus = async () => {
-//       const hasCompletedOnboarding = await AsyncStorage.getItem("hasLaunched");
-//       const isAuthenticated = await AsyncStorage.getItem("isAuth");
-
-//       if (hasCompletedOnboarding === null) {
-//         setShouldShowOnboarding(true);
-//       } else if (isAuthenticated && hasCompletedOnboarding === "true") {
-//         router.push("/(tabs)/home");
-//       } else {
-//         router.replace("/(auth)/login");
-//       }
-//     };
-
-//     verifyAppLaunchStatus();
-//   }, [router]);
-
-//   return shouldShowOnboarding ? <Redirect href="/onboarding" /> : null;
-// }
-
-
 import React, { useEffect, useState } from "react";
 import { Redirect, useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
